ceeb87697158773712dec1437242e51b5d42320b
Muhammed Irfan S <irfanmhdm123@gmail.com>
2026-07-15 21:52:23 +0530

    Signup completed

--- app.js
+++ app.js
@@ -2,14 +2,48 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const userModel = require("./models/users");
 
-let app = express();
+const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello ");
-})
+app.use(cors());
+app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-})
+mongoose.connect("mongodb://irfanmhdm:irfanmhdm@ac-se39fxg-shard-00-00.1l1lwd0.mongodb.net:27017,ac-se39fxg-shard-00-01.1l1lwd0.mongodb.net:27017,ac-se39fxg-shard-00-02.1l1lwd0.mongodb.net:27017/?ssl=true&replicaSet=atlas-ly15db-shard-0&authSource=admin&appName=Cluster0");
+
+app.post("/signup", async (req, res) => {
+    try {
+        let input = req.body;
+
+        let hashedPassword = bcrypt.hashSync(input.password, 10);
+        input.password = hashedPassword;
+
+        let check = await userModel.findOne({
+            email: input.email
+        });
+
+        if (check) {
+            return res.json({
+                status: "Email already exists"
+            });
+        }
+
+        let result = new userModel(input);
+        await result.save();
+
+        res.json({
+            status: "User registered successfully"
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: "Error",
+            message: error.message
+        });
+    }
+});
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
---
+++ models/users.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    password: { type: String, required: true }
+
+  }
+)
+
+var userModel = mongoose.model("User", userSchema);
+module.exports = userModel;
